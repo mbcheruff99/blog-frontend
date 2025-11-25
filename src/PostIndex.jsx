be8cbbb88aaabe2274396964df11f700b9e7a6b5
bshin27 <125755440+bshin27@users.noreply.github.com>
2025-11-24 19:35:21 -0500

    added a handleShow function and added to PostIndex

--- src/PostIndex.jsx
+++ src/PostIndex.jsx
@@ -2,13 +2,13 @@ export function PostIndex(props) {
   return (
     <div id="posts-index">
       <br />
-      <h1>All posts - {props.postsProp.length} </h1>
+      <h1>All posts {props.postsProp.length} </h1>
       {props.postsProp.map((post) => (
         <div key={post.id} className="posts">
           <h2>{post.title}</h2>
           <img src={post.image} />
           <p>{post.body}</p>
-          <button onClick={() => window.location.href = "https://chabad.org"}> Read </button>
+          <button onClick={() => props.onShow(post)}> Read </button>
         </div>
       ))}
     </div>
--- src/PostPage.jsx
+++ src/PostPage.jsx
@@ -1,22 +1,28 @@
 import { PostIndex } from "./PostIndex";
 import { PostsNew } from "./PostsNew";
 import axios from "axios";
+import { useState, useEffect } from 'react'
 
 export function PostPage() {
-  let posts = [];
+  const [posts, setPosts] = useState([]);
 
   const handleIndex = () => {
     axios.get("http://localhost:3000/posts.json").then((response) => {
       console.log(response.data);
-      posts = response.data
-    })
+      setPosts(response.data);
+
+    })}
+
+  const handleShow = (posts) => {
+    console.log("handleShow", posts)
   }
+
+  useEffect(handleIndex, []);
 
   return (
     <div>
       <PostsNew />
-      <PostIndex postsProp={posts} />
-      <button onClick={handleIndex}> Load Posts </button>
+      <PostIndex postsProp={posts} onShow={handleShow} />
     </div>
   );
 }
