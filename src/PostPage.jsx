8599d511784fd4d4878ba3ff0f5be0ab80121499
bshin27 <125755440+bshin27@users.noreply.github.com>
2025-11-24 20:14:50 -0500

    created a Modal popup and allowed it to come and leave when clicked

--- src/PostPage.jsx
+++ src/PostPage.jsx
@@ -2,19 +2,22 @@ import { PostIndex } from "./PostIndex";
 import { PostsNew } from "./PostsNew";
 import axios from "axios";
 import { useState, useEffect } from 'react'
+import { Modal } from "./Modal"
 
 export function PostPage() {
-  const [posts, setPosts] = useState([]);
 
-  const handleIndex = () => {
+  const [posts, setPosts] = useState([]);
+  const [isPostsShowVisible, setIsPostsShowVisible] = useState(false)
+
+  function handleIndex() {
     axios.get("http://localhost:3000/posts.json").then((response) => {
       console.log(response.data);
       setPosts(response.data);
-
     })}
 
-  const handleShow = (posts) => {
+  function handleShow(posts) {
     console.log("handleShow", posts)
+    setIsPostsShowVisible(true);
   }
 
   useEffect(handleIndex, []);
@@ -23,6 +26,9 @@ export function PostPage() {
     <div>
       <PostsNew />
       <PostIndex postsProp={posts} onShow={handleShow} />
+      <Modal show={isPostsShowVisible} onClose={() => setIsPostsShowVisible(false)}>
+        Testing 
+      </Modal>
     </div>
   );
 }
